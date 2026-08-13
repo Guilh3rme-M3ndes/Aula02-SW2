9c4f044314c862ca057fa82868d6d86fa42327b5
Guilherme Mendes <78630048+Guilh3rme-M3ndes@users.noreply.github.com>
2026-08-12 21:52:30 -0300

    Add files via upload

---
+++ CBTSWE2.Aula02/CBTSWE2.Aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CBTSWE2.Aula01;
+using CBTSWE2.Aula01.Negocio;
+using CBTSWE2.Aula01.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+
+var _repo = new LivroRepositorioCSV();
+IWebHost host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().Build();
+host.Run();
+ImprimeLista(_repo.ParaLer);
+ImprimeLista(_repo.Lendo);
+ImprimeLista(_repo.Lidos);
+
+static void ImprimeLista(ListaDeLeitura lista) {
+    Console.WriteLine(lista);
+}
---
+++ CBTSWE2.Aula02/CBTSWE2.Aula01/Startup.cs
@@ -0,0 +1,51 @@
+﻿using CBTSWE2.Aula01.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBTSWE2.Aula01
+{
+    public class Startup
+    {
+        public void configure(IApplicationBuilder app) {
+            app.Run(Roteamento);
+        }
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+            {
+                {"/Livros/ParaLer", LivrosParaLer},
+                {"/Livros/Lendo", LivrosLendo},
+                {"/Livros/Lidos", LivrosLidos }
+            };
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
+            }
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho Inexistente");
+        }
+    }
+}
